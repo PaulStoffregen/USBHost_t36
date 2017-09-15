9a2720c5de0dc537a468e3d7d81bfd4731ca53cd
Kurt Eckhardt <kurte@rockisland.com>
2017-09-14 18:41:12 -0700

    Support to query connected and id vendor and product
    Still a WIP, but trying to add support for knowing when a device is connected and when it disconnects.
    
    Put in two places.  Main USB device class, plus HID input class.
    
    Test program shows when connect and disconnect. Appears to work for Mouse and at times with Joystick.
    
    There is an issue  with some disconnects not fully working.  Appears to be problem for Keyboards,, so also problem for combo units.

--- examples/Mouse/Mouse.ino
+++ examples/Mouse/Mouse.ino
@@ -17,6 +17,10 @@ USBHIDParser hid4(myusb);
 USBHIDParser hid5(myusb);
 MouseController mouse1(myusb);
 JoystickController joystick1(myusb);
+bool keyboard1_connected = false;
+bool keyboard2_connected = false;
+bool mouse_connected = false;
+bool joystick_connected = false;
 
 void setup()
 {
@@ -31,6 +35,37 @@ void setup()
 void loop()
 {
   myusb.Task();
+  bool keyboard1_connected_now = keyboard1.connected();
+  if (keyboard1_connected != keyboard1_connected_now) {
+    keyboard1_connected = keyboard1_connected_now;
+    if (keyboard1_connected) {
+      Serial.printf("\n\n*** Keyboard 1 %04x:%04x Connected ***\n", keyboard1.idVendor(), keyboard1.idProduct());
+    } else {
+      Serial.println("\n*** Mouse disconnected ***");
+    }
+  }
+  bool keyboard2_connected_now = keyboard2.connected();
+  if (keyboard2_connected != keyboard2_connected_now) {
+    keyboard2_connected = keyboard2_connected_now;
+    if (keyboard2_connected) {
+      Serial.printf("\n\n*** Keyboard 2 %04x:%04x Connected ***\n", keyboard2.idVendor(), keyboard2.idProduct());
+    } else {
+      Serial.println("\n*** Mouse disconnected ***");
+    }
+  }
+
+  
+  bool mouse_connected_now = mouse1.connected();
+  if (mouse_connected != mouse_connected_now) {
+    mouse_connected = mouse_connected_now;
+    if (mouse_connected) {
+      Serial.printf("\n\n*** Mouse %04x:%04x Connected ***\n", mouse1.idVendor(), mouse1.idProduct());
+    } else {
+      Serial.println("\n*** Mouse disconnected ***");
+    }
+  }
+
+  
   if(mouse1.available()) {
     Serial.print("Mouse: buttons = ");
     Serial.print(mouse1.getButtons());
@@ -45,6 +80,17 @@ void loop()
     Serial.println();
     mouse1.mouseDataClear();
   }
+ 
+  bool joystick_connected_now = joystick1.connected();
+  if (joystick_connected != joystick_connected_now) {
+    joystick_connected = joystick_connected_now;
+    if (joystick_connected) {
+      Serial.printf("\n\n*** Joystick %04x:%04x Connected ***\n", joystick1.idVendor(), joystick1.idProduct());
+    } else {
+      Serial.println("\n*** Joystick disconnected ***");
+    }
+  }
+  
   if (joystick1.available()) {
     Serial.print("Joystick: buttons = ");
     Serial.print(joystick1.getButtons(), HEX);
@@ -70,14 +116,13 @@ void loop()
 
 void OnPress(int key)
 {
-	Serial.print("key '");
-	Serial.print((char)key);
-	Serial.print("'  ");
-	Serial.println(key);
-	//Serial.print("key ");
-	//Serial.print((char)keyboard1.getKey());
-	//Serial.print("  ");
-	//Serial.print((char)keyboard2.getKey());
-	//Serial.println();
+  Serial.print("key '");
+  Serial.print((char)key);
+  Serial.print("'  ");
+  Serial.println(key);
+  //Serial.print("key ");
+  //Serial.print((char)keyboard1.getKey());
+  //Serial.print("  ");
+  //Serial.print((char)keyboard2.getKey());
+  //Serial.println();
 }
-
