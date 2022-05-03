ec185ec5c8226269a57e0678c0bb685fe8cb5b9a
PaulStoffregen <paul@pjrc.com>
2022-05-03 04:53:09 -0700

    Import UsbMscFat library

--- MassStorageDriver.cpp
+++ MassStorageDriver.cpp
@@ -691,3 +691,152 @@ uint8_t msController::msProcessError(uint8_t msStatus) {
 			return msStatus;
 	}
 }
+
+
+
+#include "mscSenseKeyList.h"
+
+
+//==============================================================================
+
+bool msController::begin() {
+	m_errorCode = MS_CBW_PASS;
+	mscInit(); // Do initial init of each instance of a MSC object.
+	m_errorCode = checkConnectedInitialized();
+	if (m_errorCode) { // Check for Connected USB drive.
+		m_initDone = false;
+	} else {
+		m_initDone = true;
+	}
+	return m_initDone;
+}
+
+//------------------------------------------------------------------------------
+bool msController::readSector(uint32_t sector, uint8_t* dst) {
+	return readSectors(sector, dst, 1);
+}
+//------------------------------------------------------------------------------
+bool msController::readSectors(uint32_t sector, uint8_t* dst, size_t n) {
+	// Check if device is plugged in and initialized
+	m_errorCode = checkConnectedInitialized();
+	if (m_errorCode != MS_CBW_PASS) {
+		return false;
+	}
+	m_errorCode = msReadBlocks(sector, n, (uint16_t)msDriveInfo.capacity.BlockSize, dst);
+	if (m_errorCode) {
+		return false;
+	}
+	return true;
+}
+
+//------------------------------------------------------------------------------
+bool msController::readSectorsWithCB(uint32_t sector, size_t ns,
+	void (*callback)(uint32_t, uint8_t *), uint32_t token)
+{
+	// Check if device is plugged in and initialized
+	m_errorCode = checkConnectedInitialized();
+	if (m_errorCode != MS_CBW_PASS) {
+		return false;
+	}
+	m_errorCode = msReadSectorsWithCB(sector, ns, callback, token);
+	if (m_errorCode) {
+		return false;
+	}
+	return true;
+}
+
+
+//------------------------------------------------------------------------------
+bool msController::writeSector(uint32_t sector, const uint8_t* src) {
+	return writeSectors(sector, src, 1);
+}
+//------------------------------------------------------------------------------
+bool msController::writeSectors(uint32_t sector, const uint8_t* src, size_t n) {
+	// Check if device is plugged in and initialized
+	m_errorCode = checkConnectedInitialized();
+	if (m_errorCode != MS_CBW_PASS) {
+		return false;
+	}
+	m_errorCode = msWriteBlocks(sector, n, (uint16_t)msDriveInfo.capacity.BlockSize, src);
+	if (m_errorCode) {
+		return false;
+	}
+	return true;
+}
+
+
+#if 1
+static const char *decodeSenseKey(uint8_t senseKey) {
+	static char msg[64];
+#undef SENSE_KEY_MAP
+	switch (senseKey) {
+#define SENSE_KEY_MAP(_name_, _val_) \
+		case _val_: return #_name_ ;
+		SENSE_KEY_LIST
+	}
+#undef SENSE_KEY_MAP
+
+	snprintf(msg, sizeof(msg), "UNKNOWN SENSE KEY(%02Xh)", senseKey);
+	return msg;
+}
+
+static const char *decodeAscAscq(uint8_t asc, uint8_t ascq) {
+	static char msg[64];
+	uint16_t ascAscq = asc<<8 | ascq;
+
+	switch (ascAscq) {
+#define SENSE_CODE_KEYED(_asc_, _fmt_)
+#define SENSE_CODE(_asc_, _ascq_, _msg_) case _asc_<<8 | _ascq_: return _msg_;
+	ASC_NUM_LIST
+#undef SENSE_CODE
+#undef SENSE_CODE_KEYED
+	}
+
+#define SENSE_CODE_KEYED(_asc_, _fmt_) if (asc == _asc_) { snprintf(msg, sizeof(msg), _fmt_, ascq); return msg; }
+#define SENSE_CODE(_asc_, _ascq_, _msg_)
+	ASC_NUM_LIST
+#undef SENSE_CODE
+#undef SENSE_CODE_KEYED
+
+	snprintf(msg, sizeof(msg), "UNKNOWN ASC/ASCQ (%02Xh/%02Xh)", asc, ascq);
+	return msg;
+}
+
+//------------------------------------------------------------------------------
+static void printMscAscError(print_t* pr, msController *pDrive)
+{
+	Serial.printf(" --> Type: %s Cause: %s\n",
+		decodeSenseKey(pDrive->msSense.SenseKey),
+		decodeAscAscq(pDrive->msSense.AdditionalSenseCode,
+		pDrive->msSense.AdditionalSenseQualifier));
+
+}
+
+#undef print
+#undef println
+
+// Print error info and return.
+//
+FLASHMEM
+void MSCClass::printError(Print &p) {
+	const uint8_t err = device->errorCode();
+	if (err) {
+		if (err == 0x28) {
+			p.println(F("No USB drive detected, plugged in?"));
+		}
+		p.print(F("USB drive error: "));
+		p.print(F("0x"));
+		p.print(err, HEX);
+		p.print(F(",0x"));
+		p.print(device->errorData(), HEX);
+		printMscAscError(&p, device);
+	} else if (!mscfs.fatType()) {
+		p.println(F("Check USB drive format."));
+	}
+}
+
+
+#endif
+
+
+
--- USBHost_t36.h
+++ USBHost_t36.h
@@ -2048,7 +2048,18 @@ private:
 };
 
 //--------------------------------------------------------------------------
-class msController : public USBDriver {
+
+#include <SdFat.h>
+// Use FILE_READ & FILE_WRITE as defined by FS.h
+#if defined(FILE_READ) && !defined(FS_H)
+#undef FILE_READ
+#endif
+#if defined(FILE_WRITE) && !defined(FS_H)
+#undef FILE_WRITE
+#endif
+#include <FS.h>
+
+class msController : public USBDriver, public FsBlockDeviceInterface {
 public:
 	msController(USBHost &host) { init(); }
 	msController(USBHost *host) { init(); }
@@ -2086,6 +2097,47 @@ public:
 	uint8_t msReadSectorsWithCB(const uint32_t BlockAddress, const uint16_t Blocks, void (*callback)(uint32_t token, uint8_t* data), uint32_t token);
 	uint8_t msWriteBlocks(const uint32_t BlockAddress, const uint16_t Blocks,
                         const uint16_t BlockSize,	const void * sectorBuffer);
+	bool begin();
+public:
+	// Functions for SdFat FsBlockDeviceInterface
+	// return the number of 512 byte sectors for the whole drive
+	uint32_t sectorCount() { return msDriveInfo.capacity.Blocks; }
+	// return code for the last error.  (where is list of errors?)
+	uint8_t errorCode() const { return m_errorCode; }
+	// return error data for last error.
+	uint32_t errorData() const { return 0; }
+	// return error line for last error. Tmp function for debug.
+	uint32_t errorLine() const { return m_errorLine; }
+	// Check for busy
+	bool isBusy() { return !m_initDone && !mscTransferComplete; }
+	// Check for busy with MSC read operation
+	bool isBusyRead() { return mscTransferComplete; }
+	// Check for busy with MSC read operation
+	bool isBusyWrite() { return mscTransferComplete; }
+	// Read a USB drive's information. This contains the drive's identification
+	// information such as Manufacturer ID, Product name, Product serial
+	// number and Manufacturing date pluse more.
+	bool readUSBDriveInfo(msDriveInfo_t * driveInfo) {
+		memcpy(driveInfo, &msDriveInfo, sizeof(msDriveInfo_t));
+		return true;
+	}
+	// Read a 512 byte sector from an USB MSC drive.
+	bool readSector(uint32_t sector, uint8_t* dst);
+	// Read multiple 512 byte sectors from an USB MSC drive.
+	bool readSectors(uint32_t sector, uint8_t* dst, size_t numsectors);
+	// return USB MSC drive status.
+	uint32_t status() { return m_errorCode; }
+	// return success if sync successful. Not for user apps.
+	bool syncDevice() { return true; }
+	// Writes a 512 byte sector to an USB MSC drive.
+	bool writeSector(uint32_t sector, const uint8_t* src);
+	// Write multiple 512 byte sectors to an USB MSC drive.
+	bool writeSectors(uint32_t sector, const uint8_t* src, size_t ns);
+	// Read multiple 512 byte sectors from an USB MSC drive, using
+	// a callback per sector  TODO: this is not used by FsBlockDeviceInterface
+	bool readSectorsWithCB(uint32_t sector, size_t ns,
+		void (*callback)(uint32_t, uint8_t *), uint32_t token);
+
 protected:
 	virtual bool claim(Device_t *device, int type, const uint8_t *descriptors, uint32_t len);
 	virtual void control(const Transfer_t *transfer);
@@ -2130,9 +2182,173 @@ private:
 	uint32_t _read_sectors_token = 0;
 	uint16_t _read_sectors_remaining = 0;
 	enum {READ_CALLBACK_TIMEOUT_MS=250};
-    elapsedMillis _emlastRead;
+	elapsedMillis _emlastRead;
 	uint8_t _read_sector_buffer1[512];
 	uint8_t _read_sector_buffer2[512];
+	bool m_initDone = false;
+	uint8_t m_errorCode = MS_NO_MEDIA_ERR;
+	uint32_t m_errorLine = 0;
 };
+
+#define MSC_MAX_FILENAME_LEN 256
+
+class MSCFile : public FileImpl
+{
+private:
+	// Classes derived from File are never meant to be constructed
+	// anywhere other than open() in the parent FS class and
+	// openNextFile() while traversing a directory.
+	// Only the abstract File class which references these derived
+	// classes is meant to have a public constructor!
+	MSCFile(const FsFile &file) : mscfatfile(file), filename(nullptr) { }
+	friend class MSCClass;
+public:
+	virtual ~MSCFile(void) {
+		if (mscfatfile) mscfatfile.close();
+		if (filename) free(filename);
+	}
+#ifdef FILE_WHOAMI
+	virtual void whoami() {
+		Serial.printf("   MSCFile this=%x, refcount=%u\n",
+			(int)this, getRefcount());
+	}
+#endif
+	virtual size_t write(const void *buf, size_t size) {
+		return mscfatfile.write(buf, size);
+	}
+	virtual int peek() {
+		return mscfatfile.peek();
+	}
+	virtual int available() {
+		return mscfatfile.available();
+	}
+	virtual void flush() {
+		mscfatfile.flush();
+	}
+	virtual size_t read(void *buf, size_t nbyte) {
+		return mscfatfile.read(buf, nbyte);
+	}
+	virtual bool truncate(uint64_t size=0) {
+		return mscfatfile.truncate(size);
+	}
+	virtual bool seek(uint64_t pos, int mode = SeekSet) {
+		if (mode == SeekSet) return mscfatfile.seekSet(pos);
+		if (mode == SeekCur) return mscfatfile.seekCur(pos);
+		if (mode == SeekEnd) return mscfatfile.seekEnd(pos);
+		return false;
+	}
+	virtual uint64_t position() {
+		return mscfatfile.curPosition();
+	}
+	virtual uint64_t size() {
+		return mscfatfile.size();
+	}
+	virtual void close() {
+		if (filename) {
+			free(filename);
+			filename = nullptr;
+		}
+		mscfatfile.close();
+	}
+	virtual bool isOpen() {
+		return mscfatfile.isOpen();
+	}
+	virtual const char * name() {
+		if (!filename) {
+			filename = (char *)malloc(MSC_MAX_FILENAME_LEN);
+			if (filename) {
+				mscfatfile.getName(filename, MSC_MAX_FILENAME_LEN);
+			} else {
+				static char zeroterm = 0;
+				filename = &zeroterm;
+			}
+		}
+		return filename;
+	}
+	virtual boolean isDirectory(void) {
+		return mscfatfile.isDirectory();
+	}
+	virtual File openNextFile(uint8_t mode=0) {
+		FsFile file = mscfatfile.openNextFile();
+		if (file) return File(new MSCFile(file));
+		return File();
+	}
+	virtual void rewindDirectory(void) {
+		mscfatfile.rewindDirectory();
+	}
+#ifdef FS_FILE_SUPPORT_DATES
+	// These will all return false as only some FS support it.
+	virtual bool getAccessDateTime(uint16_t* pdate, uint16_t* ptime) {
+		return mscfatfile.getAccessDateTime(pdate, ptime);
+	}
+	virtual bool getCreateDateTime(uint16_t* pdate, uint16_t* ptime) {
+		return mscfatfile.getCreateDateTime(pdate, ptime);
+	}
+	virtual bool getModifyDateTime(uint16_t* pdate, uint16_t* ptime) {
+		return mscfatfile.getModifyDateTime(pdate, ptime);
+	}
+	virtual bool timestamp(uint8_t flags, uint16_t year, uint8_t month, uint8_t day,
+               uint8_t hour, uint8_t minute, uint8_t second) {
+		return mscfatfile.timestamp(flags, year, month, day, hour, minute, second);
+		return false;
+	}
+#endif
+
+private:
+	FsFile mscfatfile;
+	char *filename;
+};
+
+class MSCClass : public FS
+{
+public:
+	MSCClass(USBHost &host) { }
+	MSCClass(USBHost *host) { }
+	bool begin(msController *pDrive, bool setCwv = true, uint8_t part = 1) {
+		device = pDrive;
+		device->begin();
+		if (device->errorCode() != 0) return false;
+		return mscfs.begin(device, setCwv, part);
+	}
+	File open(const char *filepath, uint8_t mode = FILE_READ) {
+		oflag_t flags = O_READ;
+		if (mode == FILE_WRITE) { flags = O_RDWR | O_CREAT | O_AT_END; }
+		else if (mode == FILE_WRITE_BEGIN) { flags = O_RDWR | O_CREAT; }
+		FsFile file = mscfs.open(filepath, flags);
+		if (file) return File(new MSCFile(file));
+			return File();
+	}
+	bool exists(const char *filepath) {
+		return mscfs.exists(filepath);
+	}
+	bool mkdir(const char *filepath) {
+		return mscfs.mkdir(filepath);
+	}
+	bool rename(const char *oldfilepath, const char *newfilepath) {
+		return mscfs.rename(oldfilepath, newfilepath);
+	}
+	bool remove(const char *filepath) {
+		return mscfs.remove(filepath);
+	}
+	bool rmdir(const char *filepath) {
+		return mscfs.rmdir(filepath);
+	}
+	uint64_t usedSize() {
+		return  (uint64_t)(mscfs.clusterCount() - mscfs.freeClusterCount())
+				* (uint64_t)mscfs.bytesPerCluster();
+	}
+	uint64_t totalSize() {
+		return (uint64_t)mscfs.clusterCount() * (uint64_t)mscfs.bytesPerCluster();
+	}
+	void printError(Print &p = Serial);
+public:
+	FsVolume mscfs;      // SdFat API
+	msController *device;
+};
+
+// do not expose these defines in Arduino sketches or other libraries
+#undef MSC_MAX_FILENAME_LEN
+
+
 
 #endif
